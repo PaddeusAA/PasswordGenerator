f609973bb54ef5cdf5c8271c42021b9b7cce505b
Ginsburg_Vladislav Laptop <PaddeusVladislav@gmail.com>
2023-08-24 22:49:53 +0400

    реализован PasswordGenerator

--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class PasswordGenerator {
+    public String generatePassword(int length) {
+        // Случайный порядок символов обеспечивает большую надежность пароля
+        String characters = "QE%kN@Bvu)!tAKcO6M5x#GU$Dz4aWYqFogXPrjdTIle12p983y07mhw*S(ZJHfRV&iC^nLsb";
+
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+
+            int index = random.nextInt(characters.length());
+            password.append(characters.charAt(index));
+
+
+        }
+        return password.toString();
+    }
 }
